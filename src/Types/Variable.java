f4f5ccaa1688c925291d1b857619344889fbc47f
damebrown <db0528746968>
2018-06-09 22:53:58 +0300

    git initialization

---
+++ src/Types/Variable.java
@@ -0,0 +1,21 @@
+package Types;
+
+import java.util.*;
+
+public abstract class Variable {
+
+    /* Data members */
+
+    private boolean isFinal = false;
+    private boolean isGlobal;
+    private java.lang.String Type;
+    private java.lang.String name;
+    private java.lang.String value;
+
+    public Variable(java.lang.String type, java.lang.String name, java.lang.String value) {
+        Type = type;
+        this.name = name;
+        this.value = value;
+    }
+}
+
